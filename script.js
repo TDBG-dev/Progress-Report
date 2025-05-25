e7b8d48950ab80ae0eb7f043bc57427db5d3173d
TDBG-dev <tdbgteam@gmail.com>
2025-05-25 15:41:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -37,29 +37,4 @@ window.addEventListener("scroll", () => {
   });
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const historySection = document.querySelector("#history .content");
 
-  const imageContainer = document.createElement("div");
-  imageContainer.classList.add("image-gallery");
-
-  const imageNames = [
-    "img1", "img2", "img3", "img4", "img5",
-    "img6", "img7", "img8", "img9", "img10",
-    "img20", "img21", "img22", "img23", "img24",
-    "img25", "img26", "img32", "img33", "img34"
-  ];
-
-  imageNames.forEach(name => {
-    ["jpg", "jpeg", "png"].forEach(ext => {
-      const img = document.createElement("img");
-      img.src = `images/${name}.${ext}`;
-      img.alt = name;
-      img.className = "gallery-img";
-      img.onerror = () => img.remove(); // remove if not found
-      imageContainer.appendChild(img);
-    });
-  });
-
-  historySection.appendChild(imageContainer);
-});
